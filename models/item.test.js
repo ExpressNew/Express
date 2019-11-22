e63681b4a7adb93e2c6a3420258642c3bcf81eb8
YaraSalhi <yara.salhi@outlook.sa>
2019-11-22 08:57:37 -0800

    almostfinal

--- models/item.test.js
+++ models/item.test.js
@@ -1,57 +1,28 @@
-//Insert User into Database successfully. (Test Normal Use Case)
-//Insert User with Invalid Field. (Test on Schema)
-//Insert User without Required Field. (Test on Validation)
+const {MongoClient} = require('./item');
 
-const mongoose = require('mongoose');
-const itemsModel = require('./item');
+describe('insert', () => {
+  let connection;
+  let db;
 
-describe('item Model Test', () => {
-
-    // It's just so easy to connect to the MongoDB Memory Server 
-    // By using mongoose.connect
-    beforeAll(async () => {
-        await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-        });
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
     });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
 
-    it('create & save user successfully', async () => {
-        const validItem = new itemsModel();
-        const savedUser = await validUser.save();
-        // Object Id should be defined when successfully saved to MongoDB.
-        expect(savedUser._id).toBeDefined();
-        expect(savedUser.name).toBe(userData.name);
-        expect(savedUser.gender).toBe(userData.gender);
-        expect(savedUser.dob).toBe(userData.dob);
-        expect(savedUser.loginUsing).toBe(userData.loginUsing);
-    });
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
 
-    // Test Schema is working!!!
-    // You shouldn't be able to add in any field that isn't defined in the schema
-    it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
-        const userWithInvalidField = new UserModel({ name: 'TekLoon', gender: 'Male', nickname: 'Handsome TekLoon' });
-        const savedUserWithInvalidField = await userWithInvalidField.save();
-        expect(savedUserWithInvalidField._id).toBeDefined();
-        expect(savedUserWithInvalidField.nickkname).toBeUndefined();
-    });
+  it('should insert a doc into collection', async () => {
+    const Items = db.collection('items');
 
-    // Test Validation is working!!!
-    // It should us told us the errors in on gender field.
-    it('create user without required field should failed', async () => {
-        const userWithoutRequiredField = new UserModel({ name: 'TekLoon' });
-        let err;
-        try {
-            const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
-            error = savedUserWithoutRequiredField;
-        } catch (error) {
-            err = error
-        }
-        expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-        expect(err.errors.gender).toBeDefined();
-    });
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
 
-    
-})
+    const insertedUser = await Items.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
