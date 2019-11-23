a0510157a4c6c9340807710d22330ed3a7d04d7a
YaraSalhi <yara.salhi@outlook.sa>
2019-11-22 21:22:09 -0800

    first deployed

--- models/item.js
+++ models/item.js
@@ -41,7 +41,17 @@ sample1 = new itemsModel({
   mainComRef:"String"
 });
 
-sample1.save((error,result)=>{
+legacy = new itemsModel({
+id:1,
+name:"Supersoft Crew Neck T-Shirt",
+price:"$24.90",
+sizes:"XS,S,M,L,XL,XXL",
+colors: "red",
+reviews:["it was amasing","i like it so much","this was awesome"],
+imges:[ "https://images.express.com/is/image/expressfashion/0020_00284105_2029_f020?cache=on&wid=480&fmt=jpeg&qlt=85,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+]
+})
+legacy.save((error,result)=>{
     if(error){
         console.log("errrrrror",error
         )
@@ -50,4 +60,15 @@ sample1.save((error,result)=>{
     console.log("doneeee")
     }
     });
+
+    
+    sample1.save((error,result)=>{
+      if(error){
+          console.log("errrrrror",error
+          )
+      }
+      else{
+      console.log("doneeeeeeeeeeeeeeeeeee")
+      }
+      });
 module.exports.itemsModel = itemsModel;
